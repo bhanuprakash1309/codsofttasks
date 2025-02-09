923b1ffcb22852e734cd9c41275f2b6849edec68
bhanuprakash1309 <garagabhanuprakash09@gmail.com>
2025-02-09 22:01:58 +0530

    Update task4_rockscussors.py

--- task4_rockscussors.py
+++ task4_rockscussors.py
@@ -1,19 +1,13 @@
 options = ["Rock", "Paper", "Scissors"]
-
- 
-
+
 user_choice = input("Choose Rock, Paper, or Scissors: ")
 
 computer_choice = random.choice(options)
-
- 
-
+
 print("You chose: ", user_choice)
 
 print("Computer chose: ", computer_choice)
-
- 
-
+
 if user_choice == computer_choice:
 
     print("It's a tie!")
